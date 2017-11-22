3f5c293f27b01bd3458c36b2750beb9d1cd5075a
sagar <sagar@betterhalf.ai>
2017-11-22 14:48:32 +0530

    adding a new view

--- music/views.py
+++ music/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("<h1>This is the music app</h1>")
+
+def detail(request, album_id):
+	return HttpResponse("<h2> Details for album - " + str(album_id) + "</h2>")
