171067e987e3a8e99f89463d5b18fd6bff8757c8
sagar <sagar@betterhalf.ai>
2017-11-23 19:07:34 +0530

    404 error

--- music/views.py
+++ music/views.py
@@ -1,10 +1,18 @@
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Album
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("<h1>This is the music app</h1>")
+	all_albums = Album.objects.all()
+	context = {'all_albums': all_albums,}  
+	return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-	return HttpResponse("<h2> Details for album - " + str(album_id) + "</h2>")
-
+	try:
+		album = Album.objects.get(pk = album_id)
+	except Album.DoesNotExist:
+		raise Http404("Album does not exist")
+	return render(request, 'music/details.html', {'album': album})		
