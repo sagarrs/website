4a38540ce9d40102d4626941a5cd946596b89d37
sagar <sagar@betterhalf.ai>
2017-11-28 10:37:23 +0530

    form templates

--- music/views.py
+++ music/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, song
 
 # Create your views here.
 
@@ -11,8 +10,16 @@ def index(request):
 	return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-	try:
-		album = Album.objects.get(pk = album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album does not exist")
+	album = get_object_or_404(Album, pk = album_id)
 	return render(request, 'music/details.html', {'album': album})		
+
+def favorite(request, album_id):
+	album = get_object_or_404(Album, pk = album_id)
+	try:
+		selected_song = album.song_set.get(pk = request.POST['song'])
+	except(KeyError, song.DoesNotExist):
+		return render(request, 'music/details.html', {'album': album, 'error_message': 'Not Valid',})
+	else:
+		selected_song.is_favorite = True
+		selected_song.save()
+		return render(request, 'music/details.html', {'album':album})
